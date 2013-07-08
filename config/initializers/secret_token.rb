a382449ec4ea2998281b7e3a6459b4aaf0cc086d
Frank C. Eckert <frank.eckert@boost-project.com>
2013-07-08 23:05:03 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quant::Application.config.secret_key_base = '1b6d94e97314d530d706e9e3978f96086f2e7ea330023966e987de8cc2b319ffa807db420b6c87c6084755573484e0146326793e0ed53f9aa5112bcb647adc99'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quant::Application.config.session_store :cookie_store, key: '_quant_session'
